c143a6334b502c84d31d233d3cd13b6a5f0a67a1
SachinPattar8866 <pattarsachin10@gmail.com>
2025-08-01 14:34:44 +0530

    Initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-pink-500 text-white text-3xl font-bold">
+      Tailwind CSS is working! 🎉
+    </div>
+  );
+}
+
+export default App;
